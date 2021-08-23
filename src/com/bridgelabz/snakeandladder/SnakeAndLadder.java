b58d591df2baa49ca90e6f3ab7b7a9d19be53fd7
srinadh1304 <srinadh1304@gmail.com>
2021-08-23 07:14:16 +0530

    MODIFIED:Die number is calculated

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -6,5 +6,8 @@ public class SnakeAndLadder
 	{
 		System.out.println("Welcome to Snake and Ladder");
 		System.out.println("The Player Starts at Position 0");
+		int numberOnDie=0;
+		numberOnDie=(int)((Math.random()*100)%6)+1;
+		System.out.println("The Number on the Die is "+numberOnDie);
 	}
 }
