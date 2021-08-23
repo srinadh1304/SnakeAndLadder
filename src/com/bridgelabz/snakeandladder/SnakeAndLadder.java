17546a382c6fc6a2d9c636d534176b0166561c9e
srinadh1304 <srinadh1304@gmail.com>
2021-08-23 07:11:49 +0530

    ADDED:Player Starting position

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -5,5 +5,6 @@ public class SnakeAndLadder
 	public static void main(String args[]) 
 	{
 		System.out.println("Welcome to Snake and Ladder");
+		System.out.println("The Player Starts at Position 0");
 	}
 }
