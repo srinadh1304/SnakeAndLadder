b2c5197b882906f8483f3d430cdecb8b9c7e2fa6
srinadh1304 <srinadh1304@gmail.com>
2021-08-23 07:18:02 +0530

    ADDED:Three options for Die Roll

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -2,12 +2,33 @@ package com.bridgelabz.snakeandladder;
 
 public class SnakeAndLadder
 {
-	public static void main(String args[]) 
+	public static final int NO_PLAY=0;
+	public static final int LADDER=1;
+	public static final int SNAKE=2;
+	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to Snake and Ladder");
 		System.out.println("The Player Starts at Position 0");
-		int numberOnDie=0;
+		int numberOnDie=0,currentPosition=0;
 		numberOnDie=(int)((Math.random()*100)%6)+1;
 		System.out.println("The Number on the Die is "+numberOnDie);
+		int option=(int)(Math.random()*100)%3;
+		switch(option) 
+		{
+		case NO_PLAY:
+			 System.out.println("Option is No Play");
+			 break;
+		case LADDER:
+			System.out.println("Option is Ladder");
+			currentPosition=currentPosition+numberOnDie;
+			break;
+		case SNAKE:
+			System.out.println("Option is Snake");
+			currentPosition=currentPosition-numberOnDie;
+			break;
+		
+		}
+		
+		System.out.println("Current Position "+currentPosition);
 	}
 }
